6949973ec22bdf71faefcad2b5eb7c816bb23e3c
Marlena Flüh <m.flueh@tu-braunschweig.de>
2025-07-09 12:14:22 +0200

    update

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -34,7 +34,7 @@ export async function POST(req) {
       })),
   };
 
-  const CYPHER_GENERATION_TEMPLATE_ONTOLOGY = `
+  const CYPHER_GENERATION_TEMPLATE_TABLE_MODULE = `
   Task: Generate a cypher query that retrieves full nodes…
 
   Schema:
@@ -121,7 +121,7 @@ export async function POST(req) {
   title,
   GO-BP,
 
-    ### Pathway Properties
+  ### Pathway Properties
   $schema,
   description,
   pathway_ontology_link,
@@ -131,80 +131,44 @@ export async function POST(req) {
   pathway,
   pathway_ontology_name,
 
-  ## Metadata (the same for all nodes)
-  metadata_pid,
-  metadata_library_strategy,
-  metadata_sample_ids,
-  metadata_channel_count: 1
-  metadata_molecule_ch1,
-  metadata_contact_zip_postal_code,
-  metadata_last_update_date,
-  metadata_geo_accessions,
-  metadata_submission_date,
-  metadata_contact_name,
-  metadata_series_id,
-  metadata_taxid_ch1: 9606
-  metadata_library_source,
-  metadata_titles,
-  metadata_extract_protocol_ch1,
-  metadata_organism_ch1,
-  metadata_status,
-  metadata_contact_institute,
-  metadata_contact_country,
-  metadata_library_selection,
-  metadata_contact_phone,
-  metadata_type,
-  metadata_data_processing,
-  metadata_source_name_ch1,
-  metadata_contact_state,
-  metadata_characteristics_ch1,
-  metadata_contact_email,
-  metadata_platform_id,
-  metadata_instrument_model,
-  metadata_data_row_count: 0
-  metadata_contact_address,
-  metadata_contact_city,
+  ### Dataset Properties
+  metadata_Series_contact_phone
+  metadata_Series_platform_id
+  metadata_Series_sample_taxid
+  metadata_Series_contact_city
+  metadata_Series_platform_taxid
+  $schema
+  metadata_Series_pubmed_id
+  metadata_Series_relation 
+  description
+  metadata_Series_sample_id
+  pid
+  metadata_Series_geo_accession
+  metadata_Series_contact_address
+  title
+  metadata_Series_platform_organism
+  metadata_Series_summary
+  metadata_Series_contact_zip/postal_code
+  metadata_Series_supplementary_file
+  metadata_Series_contact_name
+  metadata_Series_contact_institute
+  metadata_Series_contact_country
+  metadata_Series_submission_date
+  metadata_Series_typ
+  metadata_Series_last_update_date5"
+  metadata_Series_sample_organism
+  metadata_Series_title
+  metadata_Series_status
+  metadata_Series_overall_design
+  metadata_Series_contact_email
+  metadata_Series_contact_state
 
-  ## Relationship Types
-
-  cypher
-  (:Gene)-[:HAS_GO_BP]->(:GO_BP)
-  (:Gene)-[:HAS_PATHWAY]->(:Pathway)
-
-  
-  Return the entire node.
-
-  The question is:
-  {question}
-  `;
-
-  const CYPHER_GENERATION_TEMPLATE = `
-  Task: Generate a cypher query that retrieves full nodes…
-
-  Schema:
-  {schema}
-
-  ## Node Types & Their Properties
-
-
-  ## Metadata (all nodes share this metadata as properties!)
+ ## Metadata (Pathway, GO_BP and Gene nodes share these properties!)
   metadata_pid,metadata_library_strategy,metadata_sample_ids,
   metadata_channel_count: 1metadata_molecule_ch1,metadata_contact_zip_postal_code,metadata_last_update_date,metadata_geo_accessions,metadata_submission_date,metadata_contact_name,metadata_series_id,
   metadata_taxid_ch1: 9606metadata_library_source,metadata_titles,metadata_extract_protocol_ch1,metadata_organism_ch1,metadata_status,metadata_contact_institute,metadata_contact_country,metadata_library_selection,metadata_contact_phone,metadata_type,metadata_data_processing,metadata_source_name_ch1,metadata_contact_state,metadata_characteristics_ch1,metadata_contact_email,metadata_platform_id,metadata_instrument_model,
   metadata_data_row_count: 0metadata_contact_address,metadata_contact_city,
 
-  ### Pathway Properties
-  $schema,description,pid,title,pathway,
-
-  ### Dataset Properties
-  $schema, description, pid, title
-
-  ### Gene Properties
-  map_location,Synonyms,gene,FDR,GO-BP,p_value,GO-CC,pathway,$schema,pid,log2(fold_change),GO-MF,GroupA_FPKM,gene_id,GroupB_FPKM,description,title,strand,biotype,chromosome,dbXrefs,Fold_change,Regulation,GeneID,locus,  
-
-  ### GO_BP Properties
-  $schema,description,pid,title,GO-BP,
-
   ## Relationship Types
 
   cypher
@@ -212,7 +176,7 @@ export async function POST(req) {
   (:Gene)-[:HAS_PATHWAY]->(:Pathway)
 
   
-  Return the entire node.
+  Return the entire node, not only a term.
 
   The question is:
   {question}
@@ -244,10 +208,36 @@ export async function POST(req) {
   pathway_ontology_name: "NCIT"
 
   ### Dataset Properties
+  metadata_Series_contact_phone: "17839914360"
+  metadata_Series_platform_id: "GPL20301"
+  metadata_Series_sample_taxid: "9606"
+  metadata_Series_contact_city: "Beijing"
+  metadata_Series_platform_taxid: "9606"
   $schema: "http://json-schema.org/draft-07/schema#"
+  metadata_Series_pubmed_id: "39717085"
+  metadata_Series_relation: "BioProject: https://www.ncbi.nlm.nih.gov/bioproject/PRJNA1180550"
   description: "Study object (such as patient, gene or cell) related information"
-  pid: 
+  metadata_Series_sample_id: ["GSM8605905", "GSM8605906", "GSM8605907", "GSM8605908", "GSM8605909", "GSM8605910", "GSM8605911", "GSM8605912"]
+  pid: "10.5281/zenodo.286719"
+  metadata_Series_geo_accession: "GSE280797"
+  metadata_Series_contact_address: "No.52 Fucheng Road, Haidian District"
   title: "Dataset"
+  metadata_Series_platform_organism: "Homo sapiens"
+  metadata_Series_summary: "The efficacy of hepatic arterial infusion chemotherapy (HAIC) with oxaliplatin (OXA) and 5-fluorouracil (5-Fu) for treating advanced perihilar cholangiocarcinoma (pCCA) has been demonstrated, yet the survival benefits of HAIC for pCCA patients vary. Here, we aimed to screen out HAIC resistance-related bile microRNAs (miRNAs) and explore the functions of specific bile miRNAs in pCCA based on high-throughput sequencing. Levels of bile miR-532-3p,miR-1250-5p, and miR-4772-5p were related to the survival of advanced pCCA patients after HAIC. However, only overexpression of miR-532-3p promoted OXA/5-Fu resistance, and downregulation of its expression improved sensitivity to OXA/5-Fu. Mechanistic investigations revealed secreted protein acidic and rich in cysteine (SPARC) as the direct target of miR-532-3p. Our study reveals that bile miR-532-3p, miR-1250-5p, and miR-4772-5p may serve as survival biomarkers in advanced pCCA patients after HAIC and that bile miR-532-3p promotes resistance to HAIC with OXA and 5-Fu via negatively regulating SPARC expression."
+  metadata_Series_contact_zip/postal_code: "100142"
+  metadata_Series_supplementary_file: "ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE280nnn/GSE280797/suppl/GSE280797_Processed_data.xlsx"
+  metadata_Series_contact_name: "Haizhen Du"
+  metadata_Series_contact_institute: "Peking University Cancer Hospital and Institute"
+  metadata_Series_contact_country: "China"
+  metadata_Series_submission_date: "Oct 31 2024"
+  metadata_Series_type: "Expression profiling by high throughput sequencing"
+  metadata_Series_last_update_date: "Jan 30 2025"
+  metadata_Series_sample_organism: "Homo sapiens"
+  metadata_Series_title: "Screening and molecular mechanism research on bile microRNAs associated with chemotherapy efficacy in perihilar cholangiocarcinoma"
+  metadata_Series_status: "Public on Nov 02 2024"
+  metadata_Series_overall_design: "A retrospective review of the Hospital Information System identified a total of 31 advanced pCCA patients who received first-line HAIC treatment from June 2015 to June 2016 at our center, of which bile samples were obtained from 18 patients prior to HAIC treatment. According to the complete survival follow-up, all of the patients died, with the following survival distribution: OS < 6 months, n = 7; OS between 6 months and 12 months, n = 4; and OS > 12 months, n = 7. We ultimately selected the bile samples of four patients among the seven with better prognosis (OS > 12 months; group A) and four patients among the seven with dismal prognosis (OS < 6 months; group B) from the biobank according to the patients’ baseline characteristics and results of RNA quality control. The bile samples (15 mL) were obtained by percutaneous transhepatic cholangial drainage (PTCD) within 7 days before the first cycle of the HAIC procedure. Centrifugation of the fresh bile at 2,000 rpm for 10 minutes yielded a supernatant, which was then stored in a freezer at −80°C. Patients signed informed consent forms for clinical specimen collection prior to PTCD. The sequencing of mRNAs was performed by CloudSeq Biotech Inc. (Shanghai, China). After removing rRNA, a NEBNext® Ultra™ II Directional RNA Library Prep Kit (New England Biolabs, Inc., MA, USA) was used to construct RNA libraries. High-throughput sequencing was performed using an Illumina HiSeq instrument. Differentially expressed mRNAs were filtered by |Fold change| ≥ 2.0 and P value < 0.05. Finally, Gene Ontology (GO) and pathway enrichment analyses were also performed for differentially expressed mRNAs."
+  metadata_Series_contact_email: "2111110621@stu.pku.edu.cn"
+  metadata_Series_contact_state: "Beijing"
 
   ### Gene Properties
   map_location: "7q21.3"
@@ -297,7 +287,7 @@ export async function POST(req) {
   `;
 
 
-  const CYPHER_GENERATION_TEMPLATE_Non_FAIR = `
+  const CYPHER_GENERATION_TEMPLATE_TABLE_MODULE_Non_FAIR = `
   Task: Generate a cypher query that retrieves full nodes…
 
   Schema:
@@ -507,7 +497,7 @@ export async function POST(req) {
     // First attempt with primary template
     console.log('Running initial Cypher generation');
     let { result: answer, intermediateSteps } = await runGraphQA(
-      new PromptTemplate({ template: TEXT_MODULE, inputVariables: ["schema", "question"] })
+      new PromptTemplate({ template: CYPHER_GENERATION_TEMPLATE_TABLE_MODULE, inputVariables: ["schema", "question"] })
     );
 
     let nodeData = intermediateSteps?.[1]?.context || [];
@@ -520,7 +510,7 @@ export async function POST(req) {
     if (shouldFallback) {
       console.warn('⚠️ No results, retrying with fallback template');
       ({ result: answer, intermediateSteps } = await runGraphQA(
-        new PromptTemplate({ template: TEXT_MODULE, inputVariables: ["schema", "question"] })
+        new PromptTemplate({ template: CYPHER_GENERATION_TEMPLATE_TABLE_MODULE, inputVariables: ["schema", "question"] })
       ));
       nodeData = intermediateSteps?.[1]?.context || [];
       console.log('Fallback Answer:', answer);
